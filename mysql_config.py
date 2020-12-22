5d135a77e6d305b6bc46aa6257c566eeedd313eb
Jang Whan Han <35212672+orajhan@users.noreply.github.com>
2020-12-22 11:42:30 +0800

    Update mysql_config.py

--- mysql_config.py
+++ mysql_config.py
@@ -5,9 +5,7 @@ mysql = MySQL()
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'AWSome123'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'YourPassword'
 app.config['MYSQL_DATABASE_DB'] = 'ociprice'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
-
-#docker run --privileged --restart always --name mysql8.0 -v /Users/jangwhan/mysql_data/8.0:/var/lib/mysql -p 3306:3306 -d -e MYSQL_ROOT_PASSWORD=AWSome123 mysql:8.0
